cf7dd83d08a5a907f530a9191529c780aa45f2ea
wang_bo <87056038+wamgbo@users.noreply.github.com>
2021-12-27 22:27:20 +0800

    Update a034: 二進位制轉換(未完).cpp

--- cpp/a034: 二進位制轉換(未完).cpp
+++ cpp/a034: 二進位制轉換(未完).cpp
@@ -1,13 +1,8 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main (){
-	int i,sum=0;
-	char b[20];
-	string a;
-	while (cin>>a){
-		for(i=0;i<=a.size()-1;i++){
-			a[i]=a[i]-'0';
-			sum+=a[i];
-		} 
-	}
-} 
+int main(){
+	int a;
+	string b;
+	cin>>b;
+	while()
+}
