5a27c916577a190012a8370e768887726380ce00
wang_bo <87056038+wamgbo@users.noreply.github.com>
2021-12-29 14:05:14 +0800

    Update a034: 二進位制轉換(未完).cpp

--- cpp/a034: 二進位制轉換(未完).cpp
+++ cpp/a034: 二進位制轉換(未完).cpp
@@ -1,8 +1,20 @@
 #include <iostream>
 using namespace std;
 int main(){
-	int a;
-	string b;
-	cin>>b;
-	while()
+	int a,b[100],i,c;
+	while(cin>>a){
+		c=0;
+		while(a>0){
+			b[c]=a%2;
+			a=a/2;
+			c++;	
+		}
+		for(i=c-1;i>=0;i--){
+			cout<<b[i];
+		}
+		cout<<endl;
+		for(i=c-1;i>=0;i--){
+			b[i]=0;	
+		}
+	}
 }
