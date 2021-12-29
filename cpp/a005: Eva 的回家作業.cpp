c42b7bb1d29dfdc6ab610016f97074cf8d1d36d8
wang_bo <87056038+wamgbo@users.noreply.github.com>
2021-12-29 19:18:57 +0800

    Update a005: Eva 的回家作業.cpp

--- cpp/a005: Eva 的回家作業.cpp
+++ cpp/a005: Eva 的回家作業.cpp
@@ -1,19 +1,17 @@
 #include <iostream>
 using namespace std;
 int main(){
-	int	row,a,b,c,d,i,x1,x2,y1,y2;
+	int	row,a,b,c,d,i,x,y1,y2;
 	cin>>row;
 	for(i=0;i<=row-1;i++){
 		cin>>a>>b>>c>>d;
-		x2=b-a;
-		x1=b/a;
-		if(x1=c/b){
-			y1=d*x1;
-			cout<<a<<' '<<b<<' '<<c<<' '<<d<<' '<<y1<<endl;
+		if(b/a==c/b){
+			x=(b/a)*d;
+			cout<<a<<' '<<b<<' '<<c<<' '<<d<<' '<<x<<endl;
 		}
-		else{
-			y2=d+x2;
-			cout<<a<<' '<<b<<' '<<c<<' '<<d<<' '<<y2<<endl;
+		else if(b-a==c-b){
+			x=(b-a)+d;
+			cout<<a<<' '<<b<<' '<<c<<' '<<d<<' '<<x<<endl;
 		}
 	} 
 }
